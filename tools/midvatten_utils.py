1e862caa0495101a521b8ac806dca9e5fac3e0f9
HenrikSpa <henrik.spangmyr@midvatten.se>
2017-03-09 15:48:40 +0100

    Updated filter_nonexisting_values_and_ask to have vertical message as default. Much better.

--- tools/midvatten_utils.py
+++ tools/midvatten_utils.py
@@ -1038,7 +1038,7 @@ def find_similar(word, wordlist, hits=5):
 
     return matches
 
-def filter_nonexisting_values_and_ask(file_data, header_value, existing_values=None, try_capitalize=False, vertical_msg_list=False, always_confirm=False):
+def filter_nonexisting_values_and_ask(file_data, header_value, existing_values=None, try_capitalize=False, vertical_msg_list=True, always_confirm=False):
     """
 
     The class NotFoundQuestion is used with 4 buttons; u'Ignore', u'Cancel', u'Ok', u'Skip'.
