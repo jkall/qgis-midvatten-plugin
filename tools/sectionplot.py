9e067236d2571253576d9b75a4582d62ea7be6ca
Simon-at-Ely <simon.oconnor@btinternet.com>
2018-10-04 12:16:06 +0100

    changed sectionplot.py to produce stratigraphy plot line object not selected and add hydrology plot.
    Signed-off-by: Simon-at-Ely <simon.oconnor@btinternet.com>

--- tools/sectionplot.py
+++ tools/sectionplot.py
@@ -1142,7 +1142,7 @@ class SectionPlot(PyQt4.QtGui.QDockWidget, Ui_SecPlotDock):#the Ui_SecPlotDock  
             annotate_txt = self.geoshort_txt
         elif self.ms.settingsdict['secplottext'] == 'capacity':
             annotate_txt = self.capacity_txt
-        elif self.ms.settingsdict['secplottext'] == 'hydroexplanations':
+        elif self.ms.settingsdict['secplottext'] == 'hydroexplanation':
             annotate_txt = self.hydro_explanation_txt
         elif self.ms.settingsdict['secplottext'] == 'development':
             annotate_txt = self.development_txt
