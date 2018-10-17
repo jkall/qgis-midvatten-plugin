26daf81796e2dc861c4c987dee2149e6442a8bd6
Simon-at-Ely <simon.oconnor@btinternet.com>
2018-10-17 21:47:08 +0100

    change to sectionplot.py attempt to allow full screen if widget undocked
    Signed-off-by: Simon-at-Ely <simon.oconnor@btinternet.com>

--- tools/sectionplot.py
+++ tools/sectionplot.py
@@ -160,8 +160,6 @@ class SectionPlot(PyQt4.QtGui.QDockWidget, Ui_SecPlotDock):#the Ui_SecPlotDock  
         self.draw_plot()
 
     def draw_plot(self): #replot
-        if self.isWindow():
-            self.undock_button_bar()
         try:
             utils.MessagebarAndLog.info(log_msg=ru(QCoreApplication.translate(u'SectionPlot', u'Plotting using settings:\n%s'))%self.secplot_templates.readable_output())
         except:
@@ -1093,14 +1091,6 @@ class SectionPlot(PyQt4.QtGui.QDockWidget, Ui_SecPlotDock):#the Ui_SecPlotDock  
         dockarea = self.parent.dockWidgetArea(self)
         self.ms.settingsdict['secplotlocation']=dockarea
 
-    def undock_button_bar(self):
-        self.titlebar = self.setTitleBarWidget(PyQt4.QtGui.QWidget())
-        # self.titlebar.maximise = self.titlebar.showMaximized()
-        # self.titlebar.addWidget(self.titlebar.maximise)
-        # PyQt4.QtGui.QDialog.__init__(self, parent)
-        # self.parent1 = None
-        self.titlebar.setWindowFlags(
-            PyQt4.QtCore.Qt.Window | PyQt4.QtCore.Qt.WindowMinimizeButtonHint | PyQt4.QtCore.Qt.WindowMaximizeButtonHint | PyQt4.QtCore.Qt.WindowCloseButtonHint);
     def upload_qgis_vector_layer(self, layer, srid=None,selected=False, mapinfo=True,Attributes=False): #from qspatialite, with a few  changes LAST ARGUMENT IS USED TO SKIP ARGUMENTS SINCE WE ONLY WANT THE GEOMETRY TO CALCULATE DISTANCES
         """Upload layer (QgsMapLayer) (optionnaly only selected values ) into current DB, in self.temptable_name (string) with desired SRID (default layer srid if None) - user can desactivate mapinfo compatibility Date importation. Return True if operation succesfull or false in all other cases"""
 
