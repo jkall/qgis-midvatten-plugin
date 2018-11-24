2e0f7a2feb565a8f4401c9d591d6d378647e388e
henrik <3uXCstAh>
2018-11-24 18:50:28 +0100

    Dev

--- tools/export_data.py
+++ tools/export_data.py
@@ -166,6 +166,7 @@ class ExportData(object):
         :return:
         """
         printlist = [[col[0] for col in self.curs.description]]
+        print(str(printlist))
         self.curs.execute("select * from %s where obsid in %s" % (tname, self.format_obsids(obsids)))
         printlist.extend(self.curs.fetchall())
         filename = os.path.join(self.exportfolder, tname + ".csv")
