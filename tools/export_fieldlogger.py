0e2f9f00f923378a825b133a7cec34bc34c07908
henrik <3uXCstAh>
2018-11-24 21:15:05 +0100

    Dev

--- tools/export_fieldlogger.py
+++ tools/export_fieldlogger.py
@@ -218,7 +218,7 @@ class ExportToFieldLogger(qgis.PyQt.QtWidgets.QMainWindow, export_fieldlogger_ui
         for index, attrs in stored_settings:
             for attr in attrs:
                 print(attr)
-                if hasattr(parameter_browser, ru(attr[0])):
+                if hasattr(parameter_browser, attr[0].decode('utf-8')):
                     setattr(parameter_browser, ru(attr[0]), ru(attr[1], keep_containers=True))
                 else:
                     utils.MessagebarAndLog.warning(log_msg=ru(QCoreApplication.translate('ExportToFieldLogger', 'Tried to load input field fields browser but the variable %s did not exist.'))%attr[0])
