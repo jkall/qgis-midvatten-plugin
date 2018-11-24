9184fe1c662fa07dd11754390cd65802e95003eb
henrik <3uXCstAh>
2018-11-24 20:08:56 +0100

    Dev

--- tools/midvatten_utils.py
+++ tools/midvatten_utils.py
@@ -321,38 +321,6 @@ class UnicodeReader(object):
         return self
 
 
-class UnicodeWriter(object):
-    """
-    A CSV writer which will write rows to CSV file "f",
-    which is encoded in the given encoding.
-    """
-
-    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
-        # Redirect output to a queue
-        self.queue = io.StringIO()
-        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
-        self.stream = f
-        self.encoder = codecs.getincrementalencoder(encoding)()
-
-    def writerow(self, row):
-        self.writer.writerow([s.encode("utf-8") for s in row])
-        # Fetch UTF-8 output from the queue ...
-        data = self.queue.getvalue()
-        if not isinstance(data, str):
-            data = str(data, 'utf-8')
-        data = str(data)
-        # ... and reencode it into the target encoding
-        data = self.encoder.encode(data)
-        # write to the target stream
-        self.stream.write(data)
-        # empty queue
-        self.queue.truncate(0)
-
-    def writerows(self, rows):
-        for row in rows:
-            self.writerow(row)
-
-
 def show_message_log(pop_error=False):
     """
     Source: qgis code
