4e5fad982a160f6422f4e4d9fbf214e4981b0c6c
henrik <3uXCstAh>
2018-11-25 10:24:57 +0100

    Dev

--- tools/import_fieldlogger.py
+++ tools/import_fieldlogger.py
@@ -188,7 +188,7 @@ class FieldloggerImport(qgis.PyQt.QtWidgets.QMainWindow, import_fieldlogger_ui_d
             print("no_duplicate")
             print(str(no_duplicate))
 
-        observations = [dict(no_duplicate) for no_duplicate in sorted(set([tuple(sorted(tuple(possible_duplicate))) for possible_duplicate in observations]), key=itemgetter(0))]
+        observations = [dict(no_duplicate) for no_duplicate in sorted(set([tuple(sorted(tuple(possible_duplicate.items()))) for possible_duplicate in observations]), key=itemgetter(0))]
         #print(str(no_duplicate))
         #observations = [dict()]
         #observations = [dict(sorted(no_duplicate, key=itemgetter(0))) for possible_duplicate in observations for no_duplicate in sorted(set([tuple(possible_duplicate.items())])]
