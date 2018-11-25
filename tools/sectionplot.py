8b138325d2393b0d6a363e6e83f2a33331707a25
henrik <3uXCstAh>
2018-11-25 16:50:12 +0100

    Dev

--- tools/sectionplot.py
+++ tools/sectionplot.py
@@ -800,19 +800,16 @@ class SectionPlot(qgis.PyQt.QtWidgets.QDockWidget, Ui_SecPlotDock):#the Ui_SecPl
                 bar_msg=ru(QCoreApplication.translate('SectionPlot', "Layer %s is missing geometry")) % layer.name())
             return False
 
-        for f in layer.getFeatures():
-            g = f.geometry()
-            print("wkt" + str(g.asWkt()))
         self.temptable_name = self.dbconnection.create_temporary_table_for_import(self.temptable_name, ['dummyfield TEXT'], ['geometry', 'LINESTRING', srid])
         #self.dbconnection.execute("INSERT into %s VALUES ('test')"%self.temptable_name)
 
-        Qsrid = QgsCoordinateReferenceSystem()
-        Qsrid.createFromId(srid)
-        if not Qsrid.isValid():  # check if crs is ok
-            utils.pop_up_info(ru(QCoreApplication.translate('SectionPlot', "Destination SRID isn't valid for table %s")) % layer.name(), self.parent)
+        #Qsrid = QgsCoordinateReferenceSystem()
+        #Qsrid.createFromId(srid)
+        #if not Qsrid.isValid():  # check if crs is ok
+        #    utils.pop_up_info(ru(QCoreApplication.translate('SectionPlot', "Destination SRID isn't valid for table %s")) % layer.name(), self.parent)
 
-            return False
-        layer.setCrs(Qsrid)
+        #    return False
+        #layer.setCrs(Qsrid)
 
         print(str(selected_features))
         feature = selected_features[0]
