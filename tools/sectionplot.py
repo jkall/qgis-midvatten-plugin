c75c99316a7aeaca528c4fce363b1c1060e72496
henrik <3uXCstAh>
2018-11-25 16:57:24 +0100

    Dev

--- tools/sectionplot.py
+++ tools/sectionplot.py
@@ -811,14 +811,17 @@ class SectionPlot(qgis.PyQt.QtWidgets.QDockWidget, Ui_SecPlotDock):#the Ui_SecPl
         #    return False
         #layer.setCrs(Qsrid)
         for f in layer.getFeatures():
-
             g = f.geometry()
-            print("featureid: " + str(f.id()) + " wkt" + str(g.asWkt()))
+            print("all:featureid: " + str(f.id()) + " wkt" + str(g.asWkt()))
+
+        for f in selected_features:
+            g = f.geometry()
+            print("selected:featureid: " + str(f.id()) + " wkt" + str(g.asWkt()))
 
         print(str(selected_features))
         feature = selected_features[0]
 
-        print("featureid:" + str(feature.id()) + " type:" + str(type(feature)))
+        #print("featureid:" + str(feature.id()) + " type:" + str(type(feature)))
         geom = feature.geometry()
         print(str(type(geom)))
         wkt = geom.asWkt()
