ce7418c3a93c2aff22ef55632b74281cc9809991
henrik <3uXCstAh>
2018-11-26 21:17:19 +0100

    Test for general exception handler

--- midvatten.py
+++ midvatten.py
@@ -414,7 +414,7 @@ class midvatten(object):
 
     @utils.general_exception_handler
     def export_spatialite(self, *args, **kwargs):
-        print("args: '{}' kwargs: '{}' ".format(str(args), str(kwargs)))
+        print("export args: '{}' kwargs: '{}' ".format(str(args), str(kwargs)))
 
         allcritical_layers = tuple(midvatten_defs.get_subset_of_tables_fr_db('obs_points') + midvatten_defs.get_subset_of_tables_fr_db('obs_lines') + midvatten_defs.get_subset_of_tables_fr_db('data_domains') + midvatten_defs.get_subset_of_tables_fr_db('default_layers') +  midvatten_defs.get_subset_of_tables_fr_db('default_nonspatlayers') )#none of these layers must be in editing mode
         err_flag = utils.verify_msettings_loaded_and_layer_edit_mode(self.iface, self.ms, allcritical_layers)#verify midv settings are loaded and the critical layers are not in editing mode
