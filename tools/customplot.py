b9940743e607a329c3b8cf873257a0fb8c285956
henrik <3uXCstAh>
2018-12-09 16:33:12 +0100

    Fixes customplot grid not showing.

--- tools/customplot.py
+++ tools/customplot.py
@@ -728,7 +728,10 @@ class plotsqlitewindow(QtWidgets.QMainWindow, customplot_ui_class):
         print("mpl.rcParams['figure.figsize'] " + str(mpl.rcParams['figure.figsize']))
         self.custplotfigure.set_size_inches(mpl.rcParams['figure.figsize'][0], mpl.rcParams['figure.figsize'][1], forward=True)
 
-        self.canvas.draw()
+        #self.canvas.updateGeometry()
+
+        #self.canvas.draw()
+        self.custplotfigure.show()
         self.plot_tabwidget.setCurrentIndex(0)
         #plt.close(self.custplotfigure)#this closes reference to self.custplotfigure
 
