14fe9eb08cb56f30878b69398dc7f13da7d4f1a8
henrik <2cXaJAyd>
2020-01-15 13:41:28 +0100

    Added zz_interlab4_obsid_assignment to datadomains.

--- definitions/midvatten_defs.py
+++ definitions/midvatten_defs.py
@@ -326,7 +326,7 @@ def get_subset_of_tables_fr_db(category='obs_points'):
     elif category == 'obs_lines':
         return ['obs_lines', 'vlf_data', 'seismic_data']
     elif category == 'data_domains':
-        return ['zz_flowtype', 'zz_meteoparam', 'zz_staff', 'zz_strat', 'zz_stratigraphy_plots', 'zz_capacity', 'zz_capacity_plots']
+        return ['zz_flowtype', 'zz_meteoparam', 'zz_staff', 'zz_strat', 'zz_stratigraphy_plots', 'zz_capacity', 'zz_capacity_plots', 'zz_interlab4_obsid_assignment']
     elif category == 'default_layers':
         return ['obs_lines', 'obs_points', 'obs_p_w_qual_field', 'obs_p_w_qual_lab', 'obs_p_w_lvl', 'obs_p_w_strat', 'w_lvls_last_geom']
     elif category == 'default_nonspatlayers':
