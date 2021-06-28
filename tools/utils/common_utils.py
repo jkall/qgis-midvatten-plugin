c6adf21e6e512924730e040a66953e52926a1c13
HenrikSpa <henrik.spangmyr@midvatten.se>
2021-06-28 07:15:58 +0000

    Added docstring.

--- tools/utils/common_utils.py
+++ tools/utils/common_utils.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ This part of the Midvatten plugin handles dates.
+                             -------------------
+        begin                : 2016-03-09
+        copyright            : (C) 2016 by HenrikSpa
+        email                : groundwatergis [at] gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
 import ast
 import copy
 import csv
